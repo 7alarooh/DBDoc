fdaf7102feb719d5d534a2587fa7fd8fe90c2e8d
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-23 16:16:03 +0400

    create checkStudentName function

--- SQL_lab4/SQLQuery_Lab4.sql
+++ SQL_lab4/SQLQuery_Lab4.sql
@@ -46,5 +46,46 @@ as return (select s.St_Fname+' '+s.St_Lname as [Full Name],
 		        on s.Dept_Id=d.Dept_Id
 		   where s.St_Id=@studentNum)
 
-select * from GetStudentAndDepartment(3)
-drop function GetStudentAndDepartment
+select * from GetStudentAndDepartment(5)
+
+--drop function GetStudentAndDepartment
+
+select * from Student
+-- 4. Create a scalar function that takes Student ID and 
+-- returns a message to user  
+--     a. If first name and Last name are null then 
+--        display 'First name & last name are null' 
+--     b. If First name is null then display 'first 
+--        name is null' 
+--     c. If Last name is null then display 'last 
+--        name is null' 
+--     d. Else display 'First name & last name are not null'
+
+create function checkStudentName (@studentNum int)
+returns nvarchar(50)
+as begin declare @firstName nvarchar(50)
+         declare @lastName nvarchar(50)
+		 declare @messageDisplay nvarchar(50)
+
+		 select @firstName=St_Fname, @lastName=St_Lname
+		 from Student
+		 where St_Id=@studentNum
+
+		 --to check
+		     if @firstName is null and @lastName is null
+		        set @messageDisplay ='First and last name are null !'
+		else if @firstName is null 
+		        set @messageDisplay ='First name is null !'
+		else if @lastName is null 
+		        set @messageDisplay ='Last name is null !'
+        else 
+		     set @messageDisplay ='First and last name are not null !'
+		return @messageDisplay
+end
+
+select dbo.checkStudentName(14)
+
+--drop function checkStudentName
+
+select * from Student
+		      
