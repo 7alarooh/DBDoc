6b80e92a79e6cbd36b05f5e5fd6e9fc389a44a27
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-23 15:10:57 +0400

    create GetMonthName() function

--- SQL_lab4/SQLQuery_Lab4.sql
+++ SQL_lab4/SQLQuery_Lab4.sql
@@ -1,1 +1,13 @@
--- SQL lab 4 
+--                          SQL lab 4 
+
+-- Note: Use ITI DB 
+
+-- 1.  Create a scalar function that takes date and returns 
+-- Month name of that date.
+create function GetMonthName (@inputDate Date)
+returns nvarchar(20)
+as
+begin declare @monthName nvarchar(20)
+set @monthName =DATENAME(month, @inputDate)
+return @monthName
+end
