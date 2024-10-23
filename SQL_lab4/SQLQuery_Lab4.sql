2c0dab987a3ae7f2036f371771633f05d1fbc513
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-23 15:49:09 +0400

    create GetStudentAndDepartment function

--- SQL_lab4/SQLQuery_Lab4.sql
+++ SQL_lab4/SQLQuery_Lab4.sql
@@ -1,6 +1,7 @@
 --                          SQL lab 4 
 
 -- Note: Use ITI DB 
+         use ITI
 
 -- 1.  Create a scalar function that takes date and returns 
 -- Month name of that date.
@@ -14,5 +15,36 @@ end
 
 select dbo.GetMonthName('2024-10-23')
 
+drop function GetMonthName
+
 -- 2.  Create a multi-statements table-valued function 
 -- that takes 2 integers and returns the values between them.
+create function GetValueBetween (@startNum int, @endNum int)
+returns @valuesTable table (value int)
+begin declare @currNum int
+  set @currNum=@startNum+1
+
+  while @currNum < @endNum
+  begin insert into @valuesTable(value)
+  values(@currNum)
+
+  set @currNum=@currNum+1
+end
+return
+end
+
+select * from GetValueBetween(5,15)
+drop function GetValueBetween
+
+-- 3.  Create inline function that takes Student No 
+-- and returns Department Name with Student full name. 
+create function GetStudentAndDepartment(@studentNum int)
+returns  table
+as return (select s.St_Fname+' '+s.St_Lname as [Full Name],
+                  d.Dept_Name as [Department Name]
+           from Student s join Department d
+		        on s.Dept_Id=d.Dept_Id
+		   where s.St_Id=@studentNum)
+
+select * from GetStudentAndDepartment(3)
+drop function GetStudentAndDepartment
