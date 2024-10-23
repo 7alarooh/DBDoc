30494706787d7812668fd68b1a1bb17f39009cc0
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-23 15:40:01 +0400

    create GetValueBetween function

--- SQL_lab4/SQLQuery_Lab4.sql
+++ SQL_lab4/SQLQuery_Lab4.sql
@@ -11,3 +11,8 @@ begin declare @monthName nvarchar(20)
 set @monthName =DATENAME(month, @inputDate)
 return @monthName
 end
+
+select dbo.GetMonthName('2024-10-23')
+
+-- 2.  Create a multi-statements table-valued function 
+-- that takes 2 integers and returns the values between them.
