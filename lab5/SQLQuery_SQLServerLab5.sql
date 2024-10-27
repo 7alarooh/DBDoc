61a1949bb1a1ced84280e6607592fced6e1ac25e
Azzak <azzaalmahrooqi79@gmail.com>
2024-10-27 11:55:06 +0400

    Add stored procedure to get student count per department

---
+++ lab5/SQLQuery_SQLServerLab5.sql
@@ -0,0 +1,18 @@
+﻿--                           SQLServer Lab 5
+
+
+-- [1] ⦁	Create a stored procedure without parameters to 
+--     show the number of students per department name.
+--     [use ITI DB] 
+create procedure getStudentCountPerDepartment
+as 
+begin
+    select d.Dept_Name AS DepartmentName,
+           COUNT(s.St_Id) AS StudentCount
+	from Department d LEFT JOIN 
+         Student s on d.Dept_Id = s.Dept_Id
+    group by d.Dept_Name
+    order by d.Dept_Name;
+end;
+
+exec getStudentCountPerDepartment;
